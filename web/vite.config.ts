fe70b3894a6b89129ddc5bc0169d20745398fa33
Redd Dayspring <redddayspring616@gmail.com>
2026-06-04 02:06:29 -0700

    add deployer

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    port: 1420
-  },
-  build: {
-    outDir: "build"
-  }
-});
+  base: '/quote-generator/',   // 🔥 IMPORTANT (repo name)
+  plugins: [svelte()]
+})
